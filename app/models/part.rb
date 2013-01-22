76340a5a9362aba6d935fe9c1ff9dc8ee2987944
Charlot <7wangsong@gmail.com>
2013-01-22 14:02:54 +0800

    ws:

--- app/models/part.rb
+++ app/models/part.rb
@@ -1,5 +1,11 @@
 class Part < ActiveRecord::Base
   attr_accessible :partNr
+  belongs_to :organisation
+    has_many :client_part_rels, :class_name=>"PartRel", :foreign_key=>"client_part_id" # org is client
+                    # :conditions=>['m_parts.part_poly_type = ?', "Supplier"]
+  has_many :supplier_part_rels, :class_name=>"PartRel", :foreign_key=>"supplier_part_id" # org is supplier
+  # belongs_to :client_part, :class_name=>"Part"#, :foreign_key=>"client_part_id"
+  # belongs_to :supplier_part, :class_name=>"Part"#, :foreign_key=>"supplier_part_id"
   # attr_accessor :orgId,:partNr
 
   # redis search -----------------------------
@@ -16,17 +22,16 @@ class Part < ActiveRecord::Base
 end
 
 
-
-class ClientPart < Part
-  # attr_accessible :title, :body
-  belongs_to :organisation_relation
-  has_one :partRelMeta, :class_name=>"PartRelMeta"
-end
-
-
-
-class SupplierPart < Part
-  # attr_accessible :title, :body
-  belongs_to :organisation_relation
-  has_one :partRelMeta, :class_name=>"PartRelMeta"
-end
+# 
+# class ClientPart < PartRel
+  # # attr_accessible :title, :body
+  # belongs_to :organisation_relation
+  # has_one :partRelMeta, :class_name=>"PartRelMeta"
+# end
+# 
+# 
+# class SupplierPart < PartRel
+  # # attr_accessible :title, :body
+  # belongs_to :organisation_relation
+  # has_one :partRelMeta, :class_name=>"PartRelMeta"
+# end
