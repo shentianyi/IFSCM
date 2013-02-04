1a847e049373ec6d0c5c1876653814745a32fd06
Charlot <7wangsong@gmail.com>
2013-02-04 13:23:45 +0800

    ws:add print selected packages method

--- app/tprinters/leoni_nbtp_dpack.rb
+++ app/tprinters/leoni_nbtp_dpack.rb
@@ -1,13 +1,24 @@
 module LeoniNbtpDpack
   @@label_keys=["SupplierNr","CPartNr","Description","Quantity","DnPackNr","Destination"]
 
-  def self.gen_data dn,orl
+  def self.gen_data dn,orl,diKeys=nil
+    if diKeys
+      return gen_selected_di_label dn,orl,diKeys
+    else
+      return gen_all_di_label dn,orl
+    end
+
+  end
+
+  private
+
+  def self.gen_all_di_label dn,orl
     dataset=[]
     if dn.items=DeliveryNote.get_children(dn.key,0,-1)[0]
       dn.items.each do |p|
         if p.items=DeliveryPackage.get_children(p.key,0,-1)[0]
           p.items.each do |i|
-            record=gen_label(p,i.key,dn.sendDate,orl)
+            record=gen_label(p,i.key,orl)
             dataset<<record
           end
         end
@@ -16,9 +27,19 @@ module LeoniNbtpDpack
     return dataset
   end
 
-  private
+  def self.gen_selected_di_label dn,orl,diKeys
+    dataset=[]
+    diKeys.each do |k|
+      if i=DeliveryItem.single_or_default(k)
+        p=DeliveryPackage.single_or_default(i.parentKey)
+        record=gen_label(p,i.key,orl)
+      dataset<<record
+      end
+    end
+    return dataset
+  end
 
-  def self.gen_label pack,packNr,sendDate,orl
+  def self.gen_label pack,packNr,orl
     record=[]
     data={}
     data[:SupplierNr]= orl.supplierNr
