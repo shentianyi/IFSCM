b81309c2cbb7c966835b5cd1058921f7af6047ce
Charlot <7wangsong@gmail.com>
2013-03-11 18:40:11 +0800

    add clean parts script

--- script/clean_mysql_data_script/clean_parts_part_rels.rb
+++ script/clean_mysql_data_script/clean_parts_part_rels.rb
@@ -1,20 +1,18 @@
 class CleanPartsPartRels
   def self.clean_parts_oart_rels ids
     ids.each do |id|
-      if pr=PartRel.where(:organisation_relation_id=>id).first
-        puts "pr:#{pr.id}"
-      pr.client_part.destroy
-      pr.supplier_part.destroy
-      pr.destroy
+      key="org:#{id}:parts:zset"
+      i=0
+      $redis.zrange(key,0,-1).each do |k|
+        if k.length-k.strip.length==1          
+          puts "#{i+=1}.-#{id}-#{k.length}-#{k.strip.length}"
+          score=$redis.zscore(key,k)          
+          $redis.zrem(key,k)
+          $redis.zadd(key,score,k)
+        end
       end
-    end
-    $redis.zrange("org:1:parts:zset",0,-1).each do |k|
-     puts "#{k.length}-#{k.strip.length}"
-     if k.length-k.strip.length==1
-       $redis.zrem("org:1:parts:zset",k)
-     end
     end
   end
 end
 
-CleanPartsPartRels.clean_parts_oart_rels [3,4]
+CleanPartsPartRels.clean_parts_oart_rels [1,2,3,4,5]
