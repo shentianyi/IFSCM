3a417eb48874a9a53597c21db4c687303586dc97
Wang Song <song.wang@brilliantech-solution.com>
2013-07-02 14:37:49 +0800

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 # It's strongly recommended to check this file into your version control system.
  
 ActiveRecord::Schema.define(:version => 20130411064842) do
- 
   create_table "cost_centers", :force => true do |t|
     t.string   "name"
     t.string   "desc"
